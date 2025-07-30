8d4a159b348dbb48598b13e3f4e859f89fea1ad9
santhoshkumarcomp <santhoshkumarcomp@gmail.com>
2025-07-30 13:39:10 +0530

    adde new cors policy

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 3000;
 
 app.use(
   cors({
-    origin: "https://bus-ticketing-frontend-oilryxke9.vercel.app", // Add your frontend URLs
+    origin: "https://bus-ticketing-frontend.vercel.app", // Add your frontend URLs
     credentials: true,
   })
 );
